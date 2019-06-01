5250b82893962515b40d53f2d21594e4ce309bb2
yyarp <m.grygorcewicz@i4b.pl>
2019-06-01 23:59:29 +0200

    working data fetch from express server

--- BentoApp/app/src/components/List/List.js
+++ BentoApp/app/src/components/List/List.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 // import './List.scss';
 
-function List() {
-  return (
-    <div className="List">
-      
-    </div>
-  );
+class List extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: [{
+        name: 'CLICK IT'
+      }]
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    axios.get('http://localhost:3000/bento')
+      .then(response => this.setState({data: response.data}))
+  }
+
+  render() {
+    return (
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick}>Click Me</button>
+        <p>{this.state.data[0].name}</p>
+      </div>
+    )
+  }
 }
 
-export default List;
+export default List
