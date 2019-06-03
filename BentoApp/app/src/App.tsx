ea03e904e02321ef6ae96067c0b9c4e6b5bc5d3e
yyarp <m.grygorcewicz@i4b.pl>
2019-06-03 22:11:56 +0200

    changed bulma to material, added header

--- BentoApp/app/src/App.tsx
+++ BentoApp/app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import List from './components/List/List'
+import Header from './components/Header/Header' 
 import './App.scss'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <List />
     </div>
   )
---
+++ BentoApp/app/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.scss'
+
+function Header() {
+  return (
+    <header className="header navbar" role="navigation" aria-label="main navigation">
+      <section className="title navbar-brand">
+        This is some header of my new and quite original app
+      </section>
+    </header>
+  )
+}
+
+export default Header
